6ffb6a245770ef66a2b51993a8b39c9754bdd502
Alejandro González Rodríguez <alejandrogonzalr@gmail.com>
2020-08-02 18:38:13 -0500

    Update GaussianNB reference name

--- 4.0/bayes.py
+++ 4.0/bayes.py
@@ -1,10 +1,10 @@
 import datasets
 from sklearn.naive_bayes import GaussianNB
 
-cb = GaussianNB()
-cb.fit(datasets.X_TRAIN, datasets.Y_TRAIN)
+clf = GaussianNB()
+clf.fit(datasets.X_TRAIN, datasets.Y_TRAIN)
 
-pred = cb.predict(datasets.X_TEST)
+pred = clf.predict(datasets.X_TEST)
 
 print("Prediction:\n", pred, end="\n\n")
 print("Test:\n", datasets.Y_TEST, end="\n\n")
